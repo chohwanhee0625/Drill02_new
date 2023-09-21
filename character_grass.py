5521cb13759c655a12eaa4d9e3be54751451f6ab
chohwanhee(lggram) <great1625@gmail.com>
2023-09-21 12:01:59 +0900

    원점에서의 원운동 완성

--- character_grass.py
+++ character_grass.py
@@ -1,11 +1,23 @@
 from pico2d import *
+import math
+
 
 def run_circle():
-    print('CIRCLE')
+    #print('CIRCLE')
+    
+    r = 200
+    for deg in range(0, 360, 5):
+        x = r*math.cos(math.radians(deg))
+        y = r*math.sin(math.radians(deg))
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.05)
+    character.draw_now(x, y)
     pass
 
 def run_rectangle():
-    print('RECTANGLE')
+    #print('RECTANGLE')
     pass
 
 
@@ -21,6 +33,6 @@ delay(1)
 while(True):
     run_circle()
     run_rectangle()
-
+    break
 
 close_canvas()
