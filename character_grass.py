8702bf43682edcac380e4f7b16faa1c2c4a73398
chohwanhee(lggram) <great1625@gmail.com>
2023-09-21 12:14:34 +0900

    바닥이동 구현

--- character_grass.py
+++ character_grass.py
@@ -6,7 +6,7 @@ def run_circle():
     #print('CIRCLE')
 
     cx, cy, r = 400, 300, 200
-    for deg in range(0, 360, 5):
+    for deg in range(-90, 270+1, 5):
         x = cx + r*math.cos(math.radians(deg))
         y = cy + r*math.sin(math.radians(deg))
         clear_canvas_now()
@@ -18,6 +18,13 @@ def run_circle():
 
 def run_rectangle():
     #print('RECTANGLE')
+
+    #bottom line
+    for x in range(50, 750+1, 5):
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        delay(0.05)
     pass
 
 
@@ -25,13 +32,9 @@ open_canvas()
 
 character = load_image('character.png')
 grass = load_image('grass.png')
-clear_canvas_now()
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
-delay(1)
 
 while(True):
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
