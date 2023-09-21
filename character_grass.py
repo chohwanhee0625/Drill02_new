888c37178032502334cd42802817f6a6f66c0b09
chohwanhee(lggram) <great1625@gmail.com>
2023-09-21 11:52:29 +0900

    사각형, 원 무한반복 성공

--- character_grass.py
+++ character_grass.py
@@ -1,14 +1,26 @@
 from pico2d import *
 
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+
 open_canvas()
 
 character = load_image('character.png')
 grass = load_image('grass.png')
-
 clear_canvas_now()
 grass.draw_now(400, 30)
 character.draw_now(400, 90)
-
-
 delay(1)
+
+while(True):
+    run_circle()
+    run_rectangle()
+
+
 close_canvas()
